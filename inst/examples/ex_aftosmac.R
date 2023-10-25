529981df432107fec117d68fa0885ce01093d0c1
Zehan Yang <zehan.yang@uconn.edu>
2023-10-25 09:14:04 -0400

    correct mistakes

--- inst/examples/ex_aftosmac.R
+++ inst/examples/ex_aftosmac.R
@@ -8,8 +8,8 @@ delta <- lymp$delta
 dat <- as.data.frame(cbind(x, y, delta))
 
 ## Fit the semi-prametric AFT model based on the rank-based approach
-fit <- aftosmac(Surv(y, delta) ~ Age + Male + Nonwhite + Year, 
+fit <- aftosmac(Surv(y, delta) ~ Age + Male + Nonwhite + Year,
                 size.pilot = 500, size.subsample = 2000,
-                sspType = "optA", model = "gehan", se = "parTrue",
+                sspType = "optA", model = "weibull", se = "parTrue",
                 combine = "estimator", data = dat)
 summary(fit)
